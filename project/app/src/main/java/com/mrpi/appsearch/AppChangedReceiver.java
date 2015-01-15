27a093e8b40e4e511645d0d17a3de0adb0fb1d22
Pieter Edelman <p.edelman@gmail.com>
2015-01-15 18:31:30 +0100

    Updated the code for detecting changes in the installed apps. It should now update the app cache on all changes, but only clean the score tables on removals.

--- project/app/src/main/java/com/mrpi/appsearch/AppChangedReceiver.java
+++ project/app/src/main/java/com/mrpi/appsearch/AppChangedReceiver.java
@@ -11,14 +11,18 @@ public class AppChangedReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent received_intent) {
     Log.d("AppSearch", "Something has changed in the apps");
-    // TODO: This code isn't valid any more
-    if (!received_intent.getBooleanExtra(Intent.EXTRA_REPLACING, false)) { // It's a real uninstall, not an upgrade
-      // URL has the form of: "package:package_url"
-      Uri data = received_intent.getData();
-      String pkg_name = data.getEncodedSchemeSpecificPart();
-      Log.d("AppSearch", "Package '" + pkg_name + "' has been uninstalled, removing it from the database");
-      AppCacheOpenHelper cache = AppCacheOpenHelper.getInstance(context);
-      cache.removePackage(pkg_name);
+
+    String action = received_intent.getAction();
+    if (action == Intent.ACTION_UNINSTALL_PACKAGE ||
+        action == Intent.ACTION_PACKAGE_FULLY_REMOVED) {
+      if (!(received_intent.getBooleanExtra(Intent.EXTRA_REPLACING, false))) { // It's no upgrade
+        // Remove package from launch cache
+        Uri data = received_intent.getData();
+        String pkg_name = data.getEncodedSchemeSpecificPart(); // URL has the form of: "package:package_url"
+        Log.d("AppSearch", "Package '" + pkg_name + "' has been uninstalled, removing it from the database");
+        AppCacheOpenHelper cache = AppCacheOpenHelper.getInstance(context);
+        cache.removePackage(pkg_name);
+      }
     }
     Intent app_index_intent = new Intent(context, AppIndexService.class);
     context.startService(app_index_intent);

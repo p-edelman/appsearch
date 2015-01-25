e3f44189e3f650b2e25f5d13b2a9cc3972c0d5bb
Pieter Edelman <p.edelman@gmail.com>
2015-01-25 08:48:31 +0100

    Completely left the idea of a resizable widget that displays the number of icons depending on its size. You have to fight the Android framework too much to do this: - it's not possible to get the number of occupied cells, you can only estimate - and even then, you can not decently space the icons without resorting to ugly hacks
    So the new idea is to just use a 4x1 widget. The only other size that makes sens is a 3x1 widget, but if needed, a separate widget of this size is trivial to add.

--- project/app/src/main/java/com/mrpi/appsearch/MostUsedWidget.java
+++ project/app/src/main/java/com/mrpi/appsearch/MostUsedWidget.java
@@ -4,10 +4,25 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabaseLockedException;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.RemoteViews;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MostUsedWidget extends AppWidgetProvider {
 
@@ -39,23 +54,34 @@ public class MostUsedWidget extends AppWidgetProvider {
                               int              widget_id) {
 
     Log.d("AppSearch", "Updating widget " + widget_id);
-    // Set up the intent that starts the StackViewService, which will
-    // provide the views for this collection.
-    Intent intent = new Intent(context, MostUsedWidgetService.class);
-
-    // Add the app widget ID to the intent extras.
-    intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widget_id);
-    intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
 
     // Instantiate the RemoteViews object for the app widget layout.
-    RemoteViews remote_views = new RemoteViews(context.getPackageName(),
-                                               R.layout.most_used_widget);
+    RemoteViews views = new RemoteViews(context.getPackageName(),
+                                        R.layout.most_used_widget);
 
-    // Set up the RemoteViews object to use a RemoteViews adapter.
-    // This adapter connects
-    // to a RemoteViewsService  through the specified intent.
-    // This is how you populate the data.
-    remote_views.setRemoteAdapter(R.id.most_used_widget_list, intent);
+    PackageManager package_manager = context.getPackageManager();
+
+    ArrayList<AppData> apps = getMostUsedApps(context);
+
+    int app_num    = 0;
+    int drawn_apps = 0;
+    while(app_num < apps.size() && drawn_apps < 4) {
+      AppData app = apps.get(app_num);
+      try {
+        ApplicationInfo app_info = package_manager.getApplicationInfo(app.package_name,
+                                                                      PackageManager.GET_META_DATA);
+        Resources resources = package_manager.getResourcesForApplication(app_info);
+        Bitmap icon         = BitmapFactory.decodeResource(resources, app_info.icon);
+        int icon_id = context.getResources().getIdentifier("widget_icon_" + drawn_apps, "id", context.getPackageName());
+        int text_id = context.getResources().getIdentifier("widget_text_" + drawn_apps, "id", context.getPackageName());
+        views.setImageViewBitmap(icon_id, icon);
+        views.setTextViewText(text_id, app.name);
+        drawn_apps++;
+      } catch (PackageManager.NameNotFoundException e) {
+        // App is not there anymore, silently ignore
+      }
+      app_num++;
+    }
 
     // The empty view is displayed when the collection has no items.
     // It should be in the same layout used to instantiate the RemoteViews
@@ -63,7 +89,93 @@ public class MostUsedWidget extends AppWidgetProvider {
     //rv.setEmptyView(R.id.stack_view, R.id.empty_view);
 
     // Instruct the widget manager to update the widget
-    widget_manager.updateAppWidget(widget_id, remote_views);
+    widget_manager.updateAppWidget(widget_id, views);
+  }
+
+  static private ArrayList<AppData> getMostUsedApps(Context context) {
+    ArrayList<AppData> apps = new ArrayList<AppData>();
+
+    SQLiteDatabase db = null;
+    try {
+      db = AppCacheOpenHelper.getInstance(context).getReadableDatabase();
+    } catch (SQLiteDatabaseLockedException e) {
+      // TODO: Handle this properly
+      Log.d("AppSearch", "Can't get a lock on the database!");
+    }
+
+    if (db != null) {
+      long time_slot = CountAndDecay.getTimeSlot();
+      int day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+      String time_slot_str = Long.toString(time_slot);
+      String day_str = Integer.toString(day);
+
+      // Create an entry for each app with the score calculated by clicks and
+      // day.
+      Map<String, AppData> app_map = new HashMap<String, AppData>();
+
+      // Get the top eight apps for this time slot and day
+      Cursor cursor = db.query(AppCacheOpenHelper.TBL_USAGE_WEEK,
+              new String[]{"public_name", "package_name", "count"},
+              "time_slot=? AND day=?",
+              new String[]{time_slot_str, day_str},
+              null, null,
+              "count DESC", "8");
+      convertCursorToAppData(cursor, app_map);
+      Log.d("MostUsed", "Got results for time and day");
+      cursor.close();
+
+      // Get the top eight apps for this time slot only
+      cursor = db.query(AppCacheOpenHelper.TBL_USAGE_DAY,
+              new String[]{"public_name", "package_name", "count"},
+              "time_slot=?",
+              new String[]{time_slot_str},
+              null, null,
+              "count DESC", "8");
+      convertCursorToAppData(cursor, app_map);
+      Log.d("MostUsed", "Got results for time");
+      cursor.close();
+
+      // Get the top eight apps overall
+      cursor = db.query(AppCacheOpenHelper.TBL_USAGE_ALL,
+              new String[]{"public_name", "package_name", "count"},
+              null, null, null, null,
+              "count DESC", "8");
+      convertCursorToAppData(cursor, app_map);
+      Log.d("MostUsed", "Got results overall");
+      cursor.close();
+
+      // Now convert it to an ArrayList and sort the results by the ratings, in
+      // descending order.
+      apps = new ArrayList<AppData>(app_map.values());
+      Collections.sort(apps, new Comparator<Object>() {
+        public int compare(Object obj1, Object obj2) {
+          return ((AppData) obj2).match_rating - ((AppData) obj1).match_rating;
+        }
+      });
+    }
+
+    return apps;
+  }
+
+  static void convertCursorToAppData(Cursor cursor, Map<String, AppData> app_map) {
+    boolean result = cursor.moveToFirst();
+    while (result) {
+      String app_name = cursor.getString(0);
+      AppData app_data = app_map.get(app_name);
+      if (app_data == null) {
+        app_data = new AppData();
+        app_data.name         = app_name;
+        app_data.package_name = cursor.getString(1);
+        app_data.match_rating = cursor.getInt(2);
+        app_map.put(app_name, app_data);
+      } else {
+        if (cursor.getInt(2) > app_data.match_rating) {
+          app_data.match_rating = cursor.getInt(2);
+        }
+      }
+      Log.d("SearchWidget", "Rating for " + app_name + " is " + app_data.match_rating);
+      result = cursor.moveToNext();
+    }
   }
 }
 

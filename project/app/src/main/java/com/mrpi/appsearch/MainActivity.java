1375fece7cbf91d32f94ee1bc01c08f917c29271
Pieter Edelman <p.edelman@gmail.com>
2015-02-05 21:30:36 +0100

    Switched the order of starting a search for popular apps and updating the app index; the popular apps search could theoretically prevent the updating from firing.

--- project/app/src/main/java/com/mrpi/appsearch/MainActivity.java
+++ project/app/src/main/java/com/mrpi/appsearch/MainActivity.java
@@ -1,15 +1,27 @@
 package com.mrpi.appsearch;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.ProgressDialog;
 import android.app.SearchManager;
-import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -58,6 +70,8 @@ public class MainActivity
 
   private CountAndDecay m_count_decay = null;
 
+  private final static String SECRET_SEND_DATA = "send database";
+
   @Override
   protected void onCreate(Bundle saved_instance) {
     // We actually never want to restore state, we should always come up as
@@ -83,7 +97,11 @@ public class MainActivity
 
       @Override
       public boolean onQueryTextSubmit(String query) {
-        doSearch(query);
+        if (query.equals(SECRET_SEND_DATA)) {
+          sendUsageData();
+        } else {
+          doSearch(query);
+        }
         return true;
       }
     };
@@ -246,6 +264,50 @@ public class MainActivity
                 // happens to the icons.
     }
     return super.onOptionsItemSelected(item);
+  }
+
+  private void sendUsageData() {
+    boolean file_copied = false;
+    File external_file = new File(getExternalCacheDir().toString(), "apps.sqlite");
+    if (external_file.exists()) {
+      external_file.delete();
+    }
+
+    // Copy the db to a readable location
+    InputStream  is = null;
+    OutputStream os = null;
+    try {
+      external_file.createNewFile();
+      DBHelper db_helper = DBHelper.getInstance(this);
+      is = new FileInputStream(db_helper.getReadableDatabase().getPath());
+      os = new FileOutputStream(external_file);
+      // Transfer bytes from in to out
+      byte[] buf = new byte[1024];
+      int len;
+      while ((len = is.read(buf)) > 0) {
+        os.write(buf, 0, len);
+      }
+      is.close();
+      os.close();
+      file_copied = true;
+    } catch (IOException e) {
+    }
+
+    if (file_copied) {
+      // Create the intent
+      Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+      intent.setType("*/*");
+      intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "AppSearch database");
+
+      // Attach the file to the intent
+      Uri db_uri = Uri.fromFile(external_file);
+      intent.putExtra(android.content.Intent.EXTRA_STREAM, db_uri);
+
+      // Run the intent
+      startActivity(Intent.createChooser(intent, "Share via"));
+    } else {
+      Log.e("AppSearch", "Couldn't copy the database file");
+    }
   }
 
   /** We need to find out how the activity was brought to the foreground; by

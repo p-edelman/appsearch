c5b4af6c779e5944ea2c7adf984bdacc1690bde2
Pieter Edelman <p.edelman@gmail.com>
2020-03-08 11:00:14 +0100

    Recycle the row views the way Android wants it

--- project/app/src/main/java/com/mrpi/appsearch/AppArrayAdapter.java
+++ project/app/src/main/java/com/mrpi/appsearch/AppArrayAdapter.java
@@ -40,7 +40,7 @@ public class AppArrayAdapter extends ArrayAdapter<AppData> {
   /** Format a single app from the list and return it as a {@link View} that can
    *  be attached to a GUI.
    *  @param position in the app list.
-   *  @param convert_view a view to reuse. This parameter is not used.
+   *  @param convert_view a view to reuse..
    *  @param parent a parent to attach the returned view to.
    *  @return a {@link View} that can be attached to a GUI.
    */
@@ -48,7 +48,7 @@ public class AppArrayAdapter extends ArrayAdapter<AppData> {
   public View getView(int position, View convert_view, ViewGroup parent) {
     while (position < m_app_data.size()) {
       AppData app_data = m_app_data.get(position);
-      View row_view = getViewForReal(app_data, parent);
+      View row_view = renderRow(app_data, convert_view, parent);
       if (row_view != null) {
         return row_view;
       } else {
@@ -65,13 +65,24 @@ public class AppArrayAdapter extends ArrayAdapter<AppData> {
     return inflater.inflate(R.layout.app_result, parent, false);
   }
 
-  public View getViewForReal(AppData app_data, ViewGroup parent) {
-    // Find the app_result XML resource and extract the views for icon and text
-    LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    View row_view = inflater.inflate(R.layout.app_result, parent, false);
-    ImageView image_view = (ImageView) row_view.findViewById(R.id.AppIcon);
-    TextView text_view   = (TextView)row_view.findViewById(R.id.AppName);
+  /** Render a single row in the list.
+    *  @param app_data The AppData object describing the app
+    *  @param parent The parent view to attach the view to
+    *  @param convert_view a possibly recycled view (see getView())
+    *  @return the rendered view, or None if rendering failed
+    */
+    private View renderRow(AppData app_data, View convert_view, ViewGroup parent) {
+    // Instantiate or recycle the row view
+    View row_view = null;
+    if (convert_view != null) {
+        row_view = convert_view;
+    } else {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        row_view = inflater.inflate(R.layout.app_result, parent, false);
+    }
 
+    // Set icon
+    ImageView image_view = (ImageView)row_view.findViewById(R.id.AppIcon);
     Drawable icon;
     try {
       // Set icon
@@ -82,6 +93,7 @@ public class AppArrayAdapter extends ArrayAdapter<AppData> {
     }
 
     // Set text; make the matching letters underlined and bold
+    TextView text_view = (TextView)row_view.findViewById(R.id.AppName);
     text_view.setText(app_data.name, TextView.BufferType.SPANNABLE);
     if (!m_render_clear && app_data.char_matches != null) {
       Spannable spannable = (Spannable)text_view.getText();
